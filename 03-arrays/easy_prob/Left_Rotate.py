8a37e63571767189e8521a880235cc25e8e2ce4b
Lakshya Wagh <lakshyawagh14@gmail.com>
2026-04-29 17:08:55 +0530

    update Rotate_array prob

--- 03-arrays/easy_prob/Left_Rotate.py
+++ 03-arrays/easy_prob/Left_Rotate.py
@@ -8,22 +8,29 @@ def Rotate_Array(arr):
 # Swapping -> not scalable for k rotations 
 #---------------------------------------------------
 
-# Better Approach 
+# Brute force Approach 
 def rotate_array(arr):
     first =arr[0]
+
     for j in range (1, len(arr)):
         arr[j-1] =arr[j]
     arr[-1] = first
+
+    # TC O(N)
+    # Sc O(1) extra space used , for implementing th algorithm O(N) 
 #----------------------------------------------------
 
 # rotate the array with k elements ( left )
+# brute Force Approach 
 
 def Rotate_k(arr,k,direction ):
     n =len(arr)
     if n == 0:
         return 
     
-    k %=n
+    # If K is larger than the size of array, e.g. k =20 , n= 7 so the roation will be ( 7 + 7 + 6 ) i.e 6 element need roatation 
+    k %=n 
+
     if direction == " left":
 
         # store first ( left ) k elements 
@@ -54,7 +61,7 @@ def Rotate_k(arr,k,direction ):
 
 
 
-# TC O(n)
+# TC -> O(k) + O(n-k) + O(k) = O(n+k)
 # SC O(k)
 
 
@@ -91,18 +98,18 @@ def RotateArray(nums, k, direction ):
         reverse(nums, k, n-1)
 
     elif direction == "left":
-        # step 1 -> Reverse first k element 
+        # step 1 -> Reverse first k element O(k)
         reverse(nums, 0, k-1)
 
-        # step 2 -> Reverse the remaining n-k elements 
+        # step 2 -> Reverse the remaining n-k elements o(n-k)
         reverse (nums, k, n-1)
 
-        # step 3 -> Reverse the entire array 
+        # step 3 -> Reverse the entire array O(n)
         reverse(nums, 0, n-1)
     
     return nums
 
-# Tc O(n)
+# Tc -> O(k) + O(n-k) + O(n) = O(2N)
 # SC O(1)
 
 #-----------------------------------------------------------------
