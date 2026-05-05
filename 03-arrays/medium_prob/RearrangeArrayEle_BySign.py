985e55a9d698b9e1e6165aa9f6b3e5e4a59e8f77
Lakshya Wagh <lakshyawagh14@gmail.com>
2026-05-05 23:59:22 +0530

    update Rearrange array elements Second

--- 03-arrays/medium_prob/RearrangeArrayEle_BySign.py
+++ 03-arrays/medium_prob/RearrangeArrayEle_BySign.py
@@ -1,5 +1,6 @@
 # Rearrange array elements by sign 
-
+# Positive numbers and negative numbers are equal in number 
+# len(pos) == len(neg)
 # Brute Force Approach 
 
 def Rearrange_Elements(nums):
@@ -62,3 +63,57 @@ def Rearrange_Elements_optimal(nums):
 nums = [ 3,1, -2, -5, 2, -4 ]
 print(Rearrange_Elements(nums))
 print(Rearrange_Elements_optimal(nums))
+
+
+
+#-------------------------------------------------
+
+# Second Variant -> len(pos) != len(neg )
+# using modfified brute force approach 
+
+
+def Rearrange_Ele (nums):
+      # List to store the positive numbers
+    pos = []
+    # List to store the negatives numbers
+    neg = []
+
+    # iterating through array and separate Positives and negatives 
+    for num in nums:
+        if num < 0:
+            neg.append(num)
+        else:
+            pos.append(num)
+
+    # Two conditions if len(pos) != len(neg)
+    # pos = [ 3,1,4,5] -> len 4
+    # neg = [-2, -1] -> len 2
+
+    if len(pos) > len(neg):
+        for i in range (len(neg)):
+            nums[2*i] = pos[i]
+            nums[2*i+1] = neg[i]
+
+        # for left overs numbers 
+        idx = len(neg) *2
+        for i in range (len(neg), len(pos)):
+            nums[idx] = pos[i]
+            idx +=1
+
+
+    # if len(pos) < len(neg)
+    else:
+        for i in range (len(pos)):
+            nums[2*i] = pos[i]
+            nums[2*i+1] = neg[i]
+
+        # for left overs numbers 
+        idx = len(pos) *2
+        for i in range (len(pos), len(neg)):
+            nums[idx] = neg[i]
+            idx +=1
+
+    return nums
+
+num = [ 3, -2, 5, 6, -1, 8]
+print(Rearrange_Ele(num))
