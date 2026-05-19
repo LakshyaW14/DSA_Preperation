95be928c5aadcbe345886007b7f01fc181d20437
Lakshya Wagh <lakshyawagh14@gmail.com>
2026-05-19 22:26:28 +0530

    update Count_Subarray Better Sol

--- 03-arrays/medium_prob/Count_Subarr_with_Sum.py
+++ 03-arrays/medium_prob/Count_Subarr_with_Sum.py
@@ -21,6 +21,23 @@ def Count_SubArr (nums,target):
 # Space Complexity: O(1) as we are not using any extra space.
 
 #---------------------------------------------------
+# Better Approach
+
+def Count_SubArr_better(nums,target):
+    count = 0
+    for i in range(len(nums)):
+        s =0
+        for j in range (i, len(nums)):
+            s += nums[j]
+            if s == target :
+                count += 1
+    return count 
+
+#Time Complexity: O(n²),We use two nested loops to check all subarrays, where n is the size of the array.
+
+# Space Complexity: O(1),Only a few extra variables are used, so constant extra space regardless of input size.
+
+#------------------------------------------
 
 nums=[3,1,2,4]
 k = 6
