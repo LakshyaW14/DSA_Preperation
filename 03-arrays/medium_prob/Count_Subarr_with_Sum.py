296f6a33a7532127467ad245e4a7f1cf2ab8b451
Lakshya Wagh <lakshyawagh14@gmail.com>
2026-05-19 22:25:43 +0530

    update Count_Subarray brute sol

---
+++ 03-arrays/medium_prob/Count_Subarr_with_Sum.py
@@ -0,0 +1,28 @@
+# Number of Subarrays with Given Sum 
+# Brute Force 
+
+def Count_SubArr (nums,target):
+    # Initialize the count of subarray occurrence 
+    count = 0
+
+    # Traverse all start indices
+    for i in range(len(nums)):
+        # Traverse from start to end
+        for j in range(i, len(nums)):
+            s = 0
+            for k in range (i,j+1):
+                s += nums[k]
+            if s == target:
+                count +=1
+    return count
+
+# Time Complexity: O(N3), where N = size of the array.We are using three nested loops here. Though all are not running for exactly N times, the time complexity will be approximately O(N3).
+
+# Space Complexity: O(1) as we are not using any extra space.
+
+#---------------------------------------------------
+
+nums=[3,1,2,4]
+k = 6
+print(Count_SubArr(nums,k))
+# print(Count_SubArr_better(nums,k))
