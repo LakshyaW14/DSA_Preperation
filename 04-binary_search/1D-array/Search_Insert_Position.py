a9b2b93d44878a59036905b996373987403471d6
Lakshya Wagh <lakshyawagh14@gmail.com>
2026-05-21 22:09:50 +0530

    add Search_Insert_Position sol

---
+++ 04-binary_search/1D-array/Search_Insert_Position.py
@@ -0,0 +1,22 @@
+# Search insert Position 
+# Lower Bound 
+def Insert_Position(nums,x):
+    low = 0
+    high = len(nums) -1
+    ans = len(nums) # default answer 
+
+    while low <= high:
+        mid = ( low + high)//2
+
+        if nums[mid] >= x:
+            ans = mid
+            # Potential Answer 
+            high = mid - 1  # look on left side 
+        
+        # look on right side 
+        else:
+            low = mid + 1
+    return ans 
+
+nums = [1,2,4,7]
+print(Insert_Position(nums, 3))
