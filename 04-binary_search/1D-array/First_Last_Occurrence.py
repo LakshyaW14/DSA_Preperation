c05b2662b992a4fe573715509154bfaa9ca95d5a
Lakshya Wagh <lakshyawagh14@gmail.com>
2026-05-23 23:52:59 +0530

    update First_last occurence optimal sol

--- 04-binary_search/1D-array/First_Last_Occurrence.py
+++ 04-binary_search/1D-array/First_Last_Occurrence.py
@@ -50,10 +50,70 @@ def Get_Occurrence_Better(nums,x):
     first = lower_bound(nums, x)
     if ( first == len(nums) ) or (nums[first] != x):
         return ( -1, -1)
-    last = upper_bound(nums, x) - 1
+    last = upper_bound(nums, x) - 1 # minus one to get the last occurring index 
+
     return (first, last)
     
-# Optimal Solution 
+
+# TC O(2 x logn)
+# SC O(1)
+
+# --------------------------------------------
+
+# Optimal Solution  -> using binary Search 
+
+
+def First(nums,x):
+    low, high = 0, len(nums)-1
+    ans = -1
+    while ( low <= high):
+        mid =( low +high)//2
+        
+        # For first index, we need to always move to left 
+        if nums[mid] == x:
+            ans = mid
+            high = mid -1       # To left 
+        
+        # then Normal BS Application 
+        elif nums[mid] < x:
+            low = mid + 1   # to right 
+        else:
+            high = mid -1
+
+    return ans 
+
+def Last (nums,x):
+    low, high = 0, len(nums) -1
+    ans  = -1 
+    while( low <= high):
+        mid = ( low + high)//2
+
+        # For Last index, we need to always move to right
+        if nums [mid] == x:
+            ans = mid 
+            low = mid + 1
+
+        # BS application 
+        elif nums[mid] < x:
+            low = mid + 1
+        else:
+            high = mid -1 
+    return ans 
+ 
+def Get_occurrence_Optimal (nums,x):
+    first_idx = First(nums, x)
+    if ( first_idx == -1) :
+        return ( -1, -1)
+    last_idx = Last(nums, x)
+    return (first_idx, last_idx )
+
+
+# TC O(logn)
+# SC O(1)
+
+#-----------------------------
+
 nums = [1,2,3,4,5,5,5,5,5,5,6,7,8]
 print(Get_Occurence(nums, 5))
 print(Get_Occurrence_Better(nums, 5))
+print(Get_occurrence_Optimal(nums, 5))
