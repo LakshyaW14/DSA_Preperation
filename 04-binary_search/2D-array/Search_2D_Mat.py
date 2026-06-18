9693f0547894fbd02db26e6ce32b1fe543e87aaa
Lakshya Wagh <lakshyawagh14@gmail.com>
2026-06-18 23:36:20 +0530

    Update Search in 2d MAt Better

--- 04-binary_search/2D-array/Search_2D_Mat.py
+++ 04-binary_search/2D-array/Search_2D_Mat.py
@@ -12,10 +12,7 @@ def search_mat(mat, target):
     return False
     
 
-mat = [ [1, 2, 3, 4], 
-       [5, 6, 7, 8],
-        [9, 10, 11, 12] ]
-target = 13
+
 
 # TC O( n x m)
 # SC O(1)
@@ -23,4 +20,42 @@ target = 13
 #---------------------------------------------------------------
 
 
-print(search_mat(mat,target))
+# Better Approach 
+
+def BS(low, high, target):
+    while ( low <= high):
+        mid = ( low + high )//2
+
+        if mid == target :
+            return True
+        elif mid > target:
+            high = mid -1
+        else:
+            low = mid + 1
+    return False
+
+def Search_Mat_Better (mat, target):
+    row = len(mat)
+    col = len(mat[0])
+
+    
+    # Loop for Traversing correct rows 
+    for i in range(row):
+        if mat[i][0] <= target <= mat[i][col-1]:
+            # If the row with target exist, use BS to find the target 
+            return BS(mat[i][0], mat[i][col-1], target)
+        
+    return False
+
+# TC O(n x logm)
+# SC O(1)
+
+#----------------------------------------------------------------
+
+
+mat = [ [1, 2, 3, 4], 
+       [5, 6, 7, 8],
+        [9, 10, 11, 12] ]
+target = 13
+
+print(Search_Mat_Better(mat,target))
