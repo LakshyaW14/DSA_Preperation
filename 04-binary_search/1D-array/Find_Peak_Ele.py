0aa2f3ebf64e2cd7e2e297440044977992c86dce
Lakshya Wagh <lakshyawagh14@gmail.com>
2026-06-22 22:33:11 +0530

    update Peak Element Optimal Sol

--- 04-binary_search/1D-array/Find_Peak_Ele.py
+++ 04-binary_search/1D-array/Find_Peak_Ele.py
@@ -15,3 +15,52 @@ def Peak_Ele(arr):
 
 #-----------------------------------------------
 
+# Optimal Solution 
+# Binary search 
+
+def Peak_Ele_Optimal (arr):
+    n = len(arr)
+   
+    # manual Edge Cases
+    if n == 1:
+        return 0
+    if arr[0] > arr[1]:
+        return 0
+    if arr[n-1] > arr[n-2]:
+        return n-1
+    
+    # 0 and n-1 idx already checked 
+    low = 1
+    high = n-2
+    while ( low <= high ):
+        mid = ( low + high )//2
+
+        # The mid is the peak Element
+        if arr[mid-1] < arr[mid] > arr[mid + 1]: 
+            return mid
+        
+        # mid is in increasing curve  low -> mid -> peak always in right 
+        elif arr[mid-1] < arr[mid]:
+            low = mid + 1
+        
+        # Mid is in Decreasing Curve  peak -> mid -> high peak always in left 
+        elif arr[mid + 1] < arr[mid]:
+            high = mid - 1
+
+        # MId is in between of two greater Elements ( or between two peaks )
+        # peak -> mid <- peak , either move to any direction 
+        # Works for both multiple and single peak 
+        else:
+            low = mid + 1 
+        
+
+    return -1 
+
+
+arr = [ 1,2,3,4,5,6,7,8,5,1]
+arr2 = [ 1]
+arr3 = [ 1,5,1,2,1]
+
+print(Peak_Ele(arr3))
+print(Peak_Ele_Optimal(arr3))
+
