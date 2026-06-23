7cd5ddae6a8f9a556fa2ea8e7978ecec565162fc
Lakshya Wagh <lakshyawagh14@gmail.com>
2026-06-23 23:44:08 +0530

    update Nth Root Optimal Sol

--- 04-binary_search/BS_on_answers/Nth_root.py
+++ 04-binary_search/BS_on_answers/Nth_root.py
@@ -15,3 +15,27 @@ def nth_Root_Brute(num, exp):
 # Sc O(1)
 
 #-----------------------
+
+# Optimal Sol 
+
+def Nth_Root_Optimal ( num, exp):
+
+    low = 1
+    high = num
+
+    while ( low <= high):
+        mid = ( low + high) // 2
+
+        if pow(mid, exp) == num:
+            return mid
+        elif pow(mid, exp ) > num:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return -1
+
+# TC O( logn)
+# SC O(1)
+
+#-------------------------
+print(Nth_Root_Optimal(num=69, exp=4))
