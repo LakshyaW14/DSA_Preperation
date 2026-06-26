8598b0b05c5390e3b665edf7dd1153fc31182688
Lakshya Wagh <lakshyawagh14@gmail.com>
2026-06-26 18:36:06 +0530

    Update Find Kth missing num Optimal

--- 04-binary_search/BS_on_answers/Kth_Missing_Pos_num.py
+++ 04-binary_search/BS_on_answers/Kth_Missing_Pos_num.py
@@ -4,6 +4,7 @@
 # Using Math 
 
 # intuition - [ 1 2 3 (4) 5 6 (7) 8 (9) (10)]
+
 def Kth_Missing_Num(arr, K):
     for num in arr:
         if num <= K:
@@ -19,7 +20,46 @@ def Kth_Missing_Num(arr, K):
 
 #-------------------------------------------
 
+# Optimal Sol 
+# Intuition 1) Find the nearby index 2) Find the number 
+
+def Find_Kth_Missing_Optimal (arr, K):
+    low = 1 
+    high = len(arr) - 1
+
+    while ( low <= high):
+        mid = ( low + high)//2
+
+        # 1-> Finding the nearby Index 
+        no_of_missing = arr[mid] - (mid+1)
+
+        # if the No_of_missing num is smaller than required 
+        # Move forward for higher num 
+        if no_of_missing < K:
+            low = mid + 1
+
+        # if Greater num move Backwards 
+        else:
+            high = mid - 1
+        # at the end ( high ) at the starting index of ans 
+
+
+        # 2 -> Find the number within the Index  
+        # arr[high] + ( k - missing)
+        # 7 + ( 5 -3 ) -> 9 ans 
+
+        # Return  (high + 1 + k ) or  low = high + 1
+        return low + k
+
+    
+# TC O(logn )
+# SC O(1)
+
+# ------------------------------
+        
 
 vec = [4,7,9,10 ]
+arr = [2,3,4,7,11]
 k = 6
 print(Kth_Missing_Num(vec, k))
+print(Find_Kth_Missing_Optimal(arr, 5))
