b5e5dc439842cf698d11ad67adf2bf7eae7456fc
Lakshya Wagh <lakshyawagh14@gmail.com>
2026-07-02 23:25:27 +0530

    Update Find Missing repeating Better

--- 03-arrays/hard_prob/Repeating_Missing_Num.py
+++ 03-arrays/hard_prob/Repeating_Missing_Num.py
@@ -26,13 +26,46 @@ def Find_Missing_Repeating(arr):
         if ( repeating != -1) and ( missing != -1):
             break
 
-    return ( repeating, missing )
+    return [repeating, missing ]
 
 # Time Complexity O( n^2) where n is the size of array, and nested Loop 
 # SC O(1) using constant amount of space for Var, regardless of input size 
 
 # ---------------------------------------------
 
+# Better - > hashing
+
+def Find_Missing_Repeating_Better(arr):
+    n = len(arr)
+    repeating, missing = -1 , -1
+
+
+    # Intialize a hash arr for counting occurrence 
+    hash_arr = [0] * (n+1)
+
+    # 
+    for num in arr:
+        hash_arr[num] += 1
+    
+    for i in range (1, n+1):
+        if hash_arr[i] == 2:
+            repeating = i
+
+        elif hash_arr[i] == 0:
+            missing = i
+
+        # early break 
+        if ( repeating != -1) and ( missing != -1):
+            break
+
+    return [ repeating, missing]
+    
+
+# TC O(2n)
+# SC O(n) ae we are using a additional Hash arr 
+
+# -----------------------------
 
 arr = [ 2,3,4,1,1,6]
 print(Find_Missing_Repeating(arr))
+print(Find_Missing_Repeating_Better(arr))
