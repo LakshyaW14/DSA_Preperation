3da96ea35343d7a5ecac75380bec81b5bd8e1b19
Lakshya Wagh <lakshyawagh14@gmail.com>
2026-07-04 12:24:17 +0530

    Update Find Repeating and Missing Optimal 2

--- 03-arrays/hard_prob/Repeating_Missing_Num.py
+++ 03-arrays/hard_prob/Repeating_Missing_Num.py
@@ -84,11 +84,9 @@ def Find_Missing_Repeating_Optimal1(arr):
 
     # Compute the difference val x - y
     val1 = s - sn
-    print(val1)
 
     # Compute the x^2 - y^2
     val2 = s2 - sn2
-    print(val2)
 
     # Calculate x + y using x^2 - y^2 // x - y
     val2 = val2 // val1
@@ -96,12 +94,87 @@ def Find_Missing_Repeating_Optimal1(arr):
     """ Calculate X and Y from X + Y and X - Y
         X = ((X + Y) + (X - Y)) / 2
         Y = X - (X - Y) """
+    
     x= ( val1 + val2)//2
     y = x - val1
 
     return [ x, y]
 
+# TC O(n) loop to calculate the sum and square of sum 
+# SC O(1) constant amount of space for variables 
+
+# -----------------------------------------
+
+
+# Optimal Sol 2 
+# Using bit manipulation and xor 
+
+# Intuition ->
+# 1. xor of arr int and n natural numbers
+# 2. Find the differentiating Bit of the Xor Num 
+# 3. put them into two parts  
+
+def Find_Missing_Repeating_Optimal2(arr):
+
+    # 1. xor of a[num] and n nums  -> O(n)
+    n = len(arr)
+    xr = 0
+    for i in range (n):
+        xr ^= arr[i]    # xor of all ele in Arr  
+        xr ^= i + 1     # xor of numbers from 1 to n 
+
+    # 2. Differentiating Bit  ( Get the Rightmost set bit in xr )
+    # e.g. xr = 4 in Bin 100 
+    # so 100 & ( 001 -> 010 -> 100) 
+    # 100 & 100 gives 1 at 2nd Bit i.e 2nd Bit is differentiating Bit 
+
+    Bit_No = 0          # O(1) using xr & -xr or O(logn ) if scanning Bits 
+    while True :
+        
+        if xr & ( (1 << Bit_No) ):
+            break
+        Bit_No += 1
+    
+    # Or Advanced way 
+    # Bit_Num = xr & ~(xr - 1 )
+
+    # 3. group numbers -> Zero Club and one Club  -> O(n)
+    one, zero = 0, 0
+    # Dividing Array Into Two Groups 
+    for i in range (n):
+        if ( arr[i] )  & (1<< Bit_No ):
+        # or arr[i] & Bitnum :
+            # one part club
+            one ^= arr[i] 
+        else:
+            # Zero Club 
+            zero ^= arr[i]
+    # Dividing numbers into Two Groups
+    for i in range (1, n+1):
+        if i & Bit_No :
+            one ^= i
+        else: 
+            zero ^= i
+    
+
+# Manual Check -> Determine Which is Repeating and Missing  O(n)
+    count = 0
+    for i in range (n):
+        if arr[i] == zero :
+            count += 1
+    if count == 2:
+        return ( zero , one )  # Repeating , missing
+    else:
+        return ( one , zero )
+
+# TC  O(n)
+# SC O(1)
+
+# -------------------------------------
+
+
 arr = [ 2,3,4,1,1,6]
-# print(Find_Missing_Repeating(arr))
-# print(Find_Missing_Repeating_Better(arr))
+print(Find_Missing_Repeating(arr))
+print(Find_Missing_Repeating_Better(arr))
 print(Find_Missing_Repeating_Optimal1(arr))
+print(Find_Missing_Repeating_Optimal2(arr))
