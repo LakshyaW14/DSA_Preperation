b96c6a9336fc96e618e3c64a0c77b59fe0c80d83
Lakshya Wagh <lakshyawagh14@gmail.com>
2026-07-04 23:09:44 +0530

    add better sol Merge two sorted arr

--- 03-arrays/hard_prob/Merge_Two_Sorted_Arr.py
+++ 03-arrays/hard_prob/Merge_Two_Sorted_Arr.py
@@ -50,6 +50,48 @@ def merge_Sorted_Arr(arr1, arr2):
 
 # ----------------------------------------------
 
+# Optimal Sol 1 without extra space 
+
+"""Two Pointer"""
+# The Two array are already sorted 
+
+# Intuition -> Compare and Swap the places 
+# the arr1 should have smalller Ele
+# arr2 should have larger ele
+
+def Merge_Sorted_Arr_Better(arr1, arr2):
+    n = len(arr1)
+    m = len(arr2)
+
+    # [ 1 , 3 , 5 , 7]left    right[ 0 , 2 , 6 , 8 , 9]m
+    left = n -1 
+    right = 0
+
+    while ( left >= 0) and ( right < m):
+
+        # if the rigthmost element of arr1 is greater
+        # than leftmost ele of arr2 
+        if arr1[left] > arr2[right]:
+            # swap 
+            arr1[left], arr2[right] = arr2[right], arr1[left]
+            left -= 1 
+            right += 1
+        
+        #if arr1[left] < arr2[right]
+        else:
+            break 
+        # rest of the ele already min 
+    
+    # sort the Arr1 and arr2
+    arr1.sort() , arr2.sort()
+    return arr1 , arr2
+
+# TC O(min(n,m)) + n logn + mlogm
+# SC O( 1)
+
+# ------------------------------------
+
 num1 = [1,3,5,7]
 num2 = [0,2,6,8,9]
 print(merge_Sorted_Arr(num1, num2))
+print(Merge_Sorted_Arr_Better(num1, num2))
