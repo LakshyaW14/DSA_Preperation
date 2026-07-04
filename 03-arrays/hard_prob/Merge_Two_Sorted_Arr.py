d85e147b49c08c189eed33a75e7e171b16a1441b
Lakshya Wagh <lakshyawagh14@gmail.com>
2026-07-04 22:49:59 +0530

    Correction Merge two sorted array Brute

--- 03-arrays/hard_prob/Merge_Two_Sorted_Arr.py
+++ 03-arrays/hard_prob/Merge_Two_Sorted_Arr.py
@@ -1,42 +1,54 @@
 # Merge Two sorted arrays without extra space 
 
-# Brute Force 
+# Brute Force with extra space 
+
 
 def merge_Sorted_Arr(arr1, arr2):
     n = len(arr1)
     m = len(arr2)
     i,j = 0, 0
-    ans =[]
-    index = 0
-    while ( i <= n) and ( j <= m):
+    temp =[]
+    # merge arr1 and arr2 to form temp in sorted order 
+    # filling the the temp with number of arr1 and arr2 in sorted order  
+    # Runs for min(n,m)
+
+    # arr1 0 to n-1
+    # arr2 0 to m-1
+
+    while ( i < n) and ( j < m):
         if ( arr1[i] <= arr2[j]):
-            ans[index] = arr1[i]
-            index += 1
+            temp.append(arr1[i])
             i += 1
         
         else:
-            ans[index] = arr2[j]
-            index += 1
+            temp.append(arr2[j])
             j += 1
-    
-    while ( i <= n):
-        ans[index] = arr1[i]
-        index += 1
+
+    # The left out element in arr1
+    while ( i < n):
+        temp.append(arr1[i])
         i += 1
 
-    while ( j <= m ):
-        ans[index] = arr2[j]
-        index += 1
+    # the left out Element in arr2
+    while ( j < m ):
+        temp.append(arr2[j])
         j += 1
 
+    # Copy Back 
+    # Filling back in arr1 and arr2 in sorted order 
     for  i in range ( n + m):
 
         if i < n :
-            arr1[i] = ans[i]
+            arr1[i] = temp[i]
         else:
-            arr2[i-n] = ans[i]
-
+            arr2[i-n] = temp[i]
+    
     return arr1, arr2
+
+# TC merge + copyback O(n+m) + O(n +m) ->> O(n+m)
+# SC temp array O(n+m)
+
+# ----------------------------------------------
 
 num1 = [1,3,5,7]
 num2 = [0,2,6,8,9]
