f857db7be6e6017362b26d948c9768fdc5ecae1d
Lakshya Wagh <lakshyawagh14@gmail.com>
2026-07-07 12:27:26 +0530

    Add Better sol Majority Ele 2

--- 03-arrays/hard_prob/Majority_Element_2.py
+++ 03-arrays/hard_prob/Majority_Element_2.py
@@ -31,6 +31,37 @@ def Majority_Element_two (arr):
 
 # ----------------------------------------------
 
+# Better approach 
+""" Hashing"""
+# intuition -> ( number, count )
+
+def Majority_Element_two_Better (arr):
+    hash_mp = {}
+    ans = []
+    n = len(arr)
+    
+    # mini times the number should appear
+    mini = ( n //3 + 1 )
+    for num in arr:
+        # Storing the element with it's occurrence
+        hash_mp[num] = hash_mp.get(num, 0) + 1
+
+        # Check Condition 
+        if hash_mp[num] == mini:
+            ans.append(num)
+        
+        # Early break 
+        if len(ans) == 2:
+            break 
+
+    return ans
+
+# TC O(n) going through every ele in arr 
+# Sc O(n) at Worst need to store every ele 
+
+# -----------------------------------------
+
 
 nums = [1, 2, 1, 1, 3,2, 2]  
 print(Majority_Element_two(nums))
+print(Majority_Element_two_Better(nums))
