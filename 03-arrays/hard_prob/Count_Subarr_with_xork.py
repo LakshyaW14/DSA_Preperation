95bffc7f6b9fbd9015cd33c8111543a87d3b3f18
Lakshya Wagh <lakshyawagh14@gmail.com>
2026-07-09 11:31:31 +0530

    Add brute count subarr with sum zero

--- 03-arrays/hard_prob/Count_Subarr_with_xork.py
+++ 03-arrays/hard_prob/Count_Subarr_with_xork.py
@@ -1,8 +1,30 @@
 # Count Subarray with xor k
 
+# Brute 
+
+def Count_Subarr_Xor_Sum_Brute (arr, k):
+    count = 0
+    # First loop 
+    for i in range (len(arr)):
+        # Second  loop 
+        for j in range (i, len(arr)):
+            xr = 0
+            # Third loop to traverse the subarray ( i -> j)
+            for k in range (i, j + 1):
+                xr ^= arr[k]
+                # Condition 
+                if xr == k:
+                    count += 1
+    return count 
+
+# TC O(n^3) nearly 
+# SC O(1)
+
+#------------------------------------
+
 # better Solution  
 
-def Count_Subarr_Xor_Sum(arr, k):
+def Count_Subarr_Xor_Sum_Better (arr, k):
     # Variable to store the count 
     count = 0
     # first loop 
@@ -26,4 +48,5 @@ def Count_Subarr_Xor_Sum(arr, k):
 
 nums = [4, 2, 2,6 ,4 ]
 
-print(Count_Subarr_Xor_Sum(nums, k=6))
+print(Count_Subarr_Xor_Sum_Brute(nums, k=6))
+print(Count_Subarr_Xor_Sum_Better(nums, k=6))
