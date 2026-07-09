f4ee5c58dabd87a0e299ed9674f7f838925c48c8
Lakshya Wagh <lakshyawagh14@gmail.com>
2026-07-09 12:08:33 +0530

    Add Optimal Largest subarr with sum zero

--- 03-arrays/hard_prob/Largest_Subarr_Sum_Zero.py
+++ 03-arrays/hard_prob/Largest_Subarr_Sum_Zero.py
@@ -30,8 +30,53 @@ def Largest_Subarr_Sum_Zero_Brute(arr):
 
 #--------------------------------------------
 
+# Optimal Sol 
+""" Prefix Sum """
+
+def Largest_Subarr_Sum_Zero_Optimal (arr):
+    # Max length so far 
+    max_length = 0
+
+    # Running Prefix sum 
+    pre_sum = 0
+
+    # Hash prefix sum -> first index seen   
+    mp = {}
+
+    # iterating over arr 
+    for i, num in enumerate (arr) :
+        # update prefix sum 
+        pre_sum += num 
+
+        # If sum is 0, subarr ( 0 -> i) has zero sum 
+        if pre_sum == 0:
+
+            # Update 
+            max_length = i + 1 
+
+        # IF not, check if it seen before
+        else:
+            # When seen, zero-sum segment between ( previous index + 1 and i )
+            if pre_sum in mp:
+                # Maximum length 
+                max_length = max ( max_length , i - mp[pre_sum] )
+
+            # first time seeing the sum, add it to map 
+            else:
+                # Record index 
+                mp[pre_sum] = i
+
+    # Return best length 
+    return max_length
+
+
+# TC O(n)
+# SC O(n) 
+#------------------------------
+
 
 
 arr = [ 15 , -2, 2, -8, 1, 7, 10, 23 ]
 
 print(Largest_Subarr_Sum_Zero_Brute(arr))
+print(Largest_Subarr_Sum_Zero_Optimal(arr))
