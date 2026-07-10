2b1826359678fd443bebb4d5f1ac9b2f8d23c5b2
Lakshya Wagh <lakshyawagh14@gmail.com>
2026-07-10 23:03:23 +0530

    Add Optimal Merge Overlapping subinterval

--- 03-arrays/hard_prob/Merge_Overlapping_subInterval.py
+++ 03-arrays/hard_prob/Merge_Overlapping_subInterval.py
@@ -4,15 +4,16 @@
 
 def Merge_Overlapping_Subinterval(arr):
     # arr to store the result
+    arr.sort()
     ans = []
     
     # intialize the start and end variable -> ( start, end )
 
     # iterating over the arr
-    for i in range (len(arr)):
+    for i in range (len(arr)):  # 0 -> n
+
         start = arr[i][0]
         end = arr[i][1]
-
         # second loop 
         for j in range ( i+1, len(arr)):
 
@@ -20,16 +21,30 @@ def Merge_Overlapping_Subinterval(arr):
             if arr[j][0] <= end:
                 # Update the end with, max possible element 
                 end = max (end, arr[j][1])
-
+            if [ start, end ] in ans :
+                continue
             else:
                 break 
         ans.append((start, end))
          
     return ans
     # intialize the start and end variable -> ( start, end )
-    
+
+# TC O(n^2)
+# SC O(1)
+
+# -------------------------------
+# Optimal sol 
+
+def Merge_Overlapping_Subinterval(arr):
+    ...
 
 
 
-intervals=[[1,3],[2,6],[8,10],[15,18]]
+# TC O(n)
+# SC O(1)
+
+
+
+intervals=[[1,3],[2,4],[2,6],[8,10],[15,18]]
 print(Merge_Overlapping_Subinterval(intervals))
