a3adb9e77526df671bcca761253350c5f0acb4a2
Lakshya Wagh <lakshyawagh14@gmail.com>
2026-07-16 22:11:07 +0530

    Add intuitive Remove parenthesis sol

--- 05-Strings/basic_strings/Remove_Outermost_Parentheses.py
+++ 05-Strings/basic_strings/Remove_Outermost_Parentheses.py
@@ -36,8 +36,45 @@ def Remove_Outermost_Parentheses(str):
 # Sc O(1) using only few variables 
 # 
 # ---------------------------------------
+
+# Intuitive approach 
+"""Stack """
+
+def Remove_Outermost_Parenthesis_(s):
+
+    # Stack to store the valid Parenthesis
+    stack = []
+    result = []
+
+    # Traversing the string 
+    for char in s:
+        if char == '(':
+            # if stack not empty , this is an inner parenthesis
+            if stack :
+                result.append(char)
+
+            # add it to stack 
+            stack.append(char)
+
+        else:
+            # Char == ')' 
+            # Pop from stack 
+            stack.pop()
+
+            # if stack not empty after popping, this is an inner parenthesis
+            if stack:
+                result.append(char)
+
+    return "".join(result)
+
+
+# TC O(n) traversing the whole string 
+# SC O(N) at worst the stack would store the n/2 char 
+# 
  
 string = "()(()())(())"
 s = "((()))"
 print(Remove_Outermost_Parentheses(string))
+print(Remove_Outermost_Parenthesis_(string))
 print(Remove_Outermost_Parentheses(s))
+print(Remove_Outermost_Parenthesis_(s))
